fb4b6221bce087fac43b146cda4b4cb50e9f306a
mottl <jan.mottl.brno@gmail.com>
2021-06-16 14:38:04 +0200

    Databaze

--- sql/data_sample_user.sql
+++ sql/data_sample_user.sql
@@ -15,5 +15,18 @@ VALUES
 
 INSERT INTO user_address (user_id, user_adresa, user_stat, user_obec_psc, user_obec_nazev)
 VALUES
-    (1, 'U potoka', '', '641 00', 'Brno'),
-    (1, 'U rybníka', '', '641 00', 'Brno');
+    (1, 'Zámecká 12', '', '664 61', 'Rebešovice'),
+    (1, 'Hrázní 333/2D,', 'CZ', '635 00', 'Brno-Kníničky'),
+    (1, 'Bezručova 2297/2', '', '678 01', 'Blansko'),
+    (1, 'Via Aurelia 111', 'IT', '570 16', 'Castiglioncello'),
+    (1, 'Museumsinsel', 'DE', '	10961', 'Berlin');
+
+INSERT INTO user_address (user_id, user_adresa, user_stat, user_obec_psc, user_obec_nazev)
+SELECT user.user_id, ua.user_adresa, ua.user_stat, ua.user_obec_psc, ua.user_obec_nazev
+FROM user,
+     (SELECT ua.user_adresa, ua.user_stat, ua.user_obec_psc, ua.user_obec_nazev
+      FROM user
+               LEFT JOIN user_address ua on user.user_id = ua.user_id
+      WHERE user.user_id = 1) ua
+WHERE user.user_id > 1
+ORDER BY user.user_id;
